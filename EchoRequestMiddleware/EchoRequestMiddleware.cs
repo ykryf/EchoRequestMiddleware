228f9249e842284b7591af0a1a7eddd735fa5a8a
Yiannis Kryfos <yianniskryfos@yahoo.com>
2021-02-01 13:23:14 +0200

    Add project files.

---
+++ EchoRequestMiddleware/EchoRequestMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EchoRequestMiddleware
+{
+    class EchoRequestMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public EchoRequestMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+
+        }
+    }
+}
